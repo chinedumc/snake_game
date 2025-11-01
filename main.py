960b23a587582dc1bec1b159262ce3a197fc3ac0
chinedumc <chibokachinedum@gmail.com>
2025-11-01 23:52:00 +0100

    Start Snake animation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake game')
+screen.tracer(0)
 
 starting_positions = [(0,0),(-20,0),(-40,0)]
 
@@ -30,6 +32,8 @@ for position in starting_positions:
 
 game_is_on = True
 while game_is_on:
+  screen.update()
+  time.sleep(0.1)
   for seg in segments:
     seg.forward(20)
 

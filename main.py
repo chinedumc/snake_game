b232eddfe1c2e7f3b7dbb78409e0c5ded35e0711
chinedumc <chibokachinedum@gmail.com>
2025-11-02 14:42:09 +0100

    Control the Snake with Arrow Keys

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,17 +8,24 @@ screen.bgcolor('black')
 screen.title('Snake game')
 screen.tracer(0)
 
-starting_positions = [(0,0),(-20,0),(-40,0)]
+# starting_positions = [(0,0),(-20,0),(-40,0)]
 
-segments = []
+# segments = []
 
+snake = Snake()
 
-for position in starting_positions:
-  new_segment = Turtle('square')
-  new_segment.color('white')
-  new_segment.penup()
-  new_segment.goto(position)
-  segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# for position in starting_positions:
+#   new_segment = Turtle('square')
+#   new_segment.color('white')
+#   new_segment.penup()
+#   new_segment.goto(position)
+#   segments.append(new_segment)
 
 # segment_1 = Turtle('square')
 # segment_1.color('white')
@@ -34,10 +42,14 @@ game_is_on = True
 while game_is_on:
   screen.update()
   time.sleep(0.1)
-  for seg in segments:
-    seg.forward(20)
 
+  snake.move()
 
+  # for seg_num in range(len(segments)-1, 0,-1):
+  #   new_x = segments[seg_num - 1].xcor()
+  #   new_y = segments[seg_num - 1].ycor()
+  #   segments[seg_num].goto(new_x, new_y)
+  # segments[0].forward(20)
 
 
 

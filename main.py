666c06fa439c00013fbdb7c32b6b1e1dd3ebad31
chinedumc <chibokachinedum@gmail.com>
2025-11-03 23:23:02 +0100

    Created Food And Detected Food Collision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+from food import Food
 from snake import Snake
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 # segments = []
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -30,20 +32,16 @@ screen.onkey(snake.right, "Right")
 # segment_1 = Turtle('square')
 # segment_1.color('white')
 
-# segment_2 = Turtle('square')
-# segment_2.color('white')
-# segment_2.goto(-20, 0)
-
-# segment_3 = Turtle('square')
-# segment_3.color('white')
-# segment_3.goto(-40, 0)
-
 game_is_on = True
 while game_is_on:
   screen.update()
   time.sleep(0.1)
 
   snake.move()
+
+  #Detect collision with food.
+  if snake.head.distance(food) < 15:
+    food.refresh()
 
   # for seg_num in range(len(segments)-1, 0,-1):
   #   new_x = segments[seg_num - 1].xcor()
